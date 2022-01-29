25783a014d9b6ad6a1b00233dffd9518263cf6e7
last <fed.lag@protonmail.ch>
2022-01-29 13:35:31 +0100

    Update SetPrivilege.cpp

--- SetPrivilege.cpp
+++ SetPrivilege.cpp
@@ -1,21 +1,20 @@
-#include "common.h"
-//#pragma comment(lib, "cmcfg32.lib")
+#include "common.hpp"
 
 bool SetPrivilege(
     _In_ HANDLE token, // won't be using RAII as we don't need the HANDLE to get closed after the function returns
     _In_ std::wstring privilege,
-    _In_ bool enableDisable   
+    _In_ bool enableDisable
 )
 {
     TOKEN_PRIVILEGES tp{};
     LUID luid;
-    
-    auto result = LookupPrivilegeValueW(NULL, privilege.c_str(), &luid);
 
-    if (!result)
+    auto success = LookupPrivilegeValueW(NULL, privilege.c_str(), &luid);
+
+    if (!success)
     {
         Error(GetLastError());
-        std::wcout << L"[-] Privilege " << privilege << L" is not assigned to the current token, can't enable it...\n";
+        std::wcout << L"[-] The call to LookupPrivilegeValueW failed...\n";
         return false;
     }
 
@@ -27,16 +26,14 @@ bool SetPrivilege(
     else
         tp.Privileges[0].Attributes = 0;
 
-    result = AdjustTokenPrivileges(token, false, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL);
-    if (!result)
+    success = AdjustTokenPrivileges(token, false, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL);
+    auto result = GetLastError(); // GetLastError tells us if the privilege has been successfully assigned
+    if (result != ERROR_SUCCESS)
     {
         Error(GetLastError());
         std::wcout << L"[-] Couldn't add privilege " << privilege << L" to the current token...\n";
         return false;
     }
-    else 
-    {
-        std::wcout << L"[+] Privilege " << privilege << L" added to the current token!\n";
-        return true;
-    }
+    std::wcout << L"[+] Privilege " << privilege << L" added to the current token!\n";
+    return true;
 }
